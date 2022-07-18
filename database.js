37606ed28d3f8ff72fa8fca3686707cd946afe7f
Deji Omoloja <dejiomoloja@Dejis-MacBook-Air.local>
2022-07-18 03:30:14 +0100

    setting up heroku postgres

--- database.js
+++ database.js
@@ -5,12 +5,16 @@ const { Client } = pkg;
 dotenv.config();
 
 const client = new Client({
-    host: 'localhost',
-    user: 'postgres',
-    port: 5432,
-    password: `${process.env.DATABASEPASSWORD}`,
-    connectionLimit: 10,
-    database: 'AbbeyBank'
+    // host: 'localhost',
+    // user: 'postgres',
+    // port: 5432,
+    // password: `${process.env.DATABASEPASSWORD}`,
+    // connectionLimit: 10,
+    // database: 'AbbeyBank'
+    connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 })
 
 export default client
--- index.js
+++ index.js
@@ -2,8 +2,10 @@ import express from 'express';
 import cors from 'cors';
 import client from './database.js'
 import userRoute from './route/user_route.js'
+import dotenv from 'dotenv' 
 
 export const app = express();
+dotenv.config();
 
 app.use(cors());
 app.use(express.json({limit: '50mb'}));
